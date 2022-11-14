75a10396eb92a049d7166a28e03ceee875331bb4
GhiwaManager <=>
2022-11-14 19:23:06 +0200

    add some update

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const userRouter = require("./userRouter");
 const authRouter = require("./authRouter");
 const messageRouter = require("./messageRouter");
 const inboxRouter = require("./inboxRouter");
+const rateLimet = require("../middleware/rateLimitMW");
 module.exports = (app) => {
-  app.use("/api/v1/user", userRouter);
-  app.use("/api/v1/auth", authRouter);
+  app.use("/api/v1/user", rateLimet, userRouter);
+  app.use("/api/v1/auth", rateLimet, authRouter);
   app.use("/api/v1/messages", messageRouter);
   app.use("/api/v1/inbox", inboxRouter);
 };
